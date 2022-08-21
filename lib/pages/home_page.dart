152a2cf8d0ff190d3977b199091020bf490728f6
diegomoraes <diegofrost96@gmail.com>
2022-08-21 00:09:17 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -53,160 +53,162 @@ class _HomePageState extends State<HomePage> {
         body: SizedBox(
           width: size.width,
           height: size.height,
-          child: ListView(
-            children: [
-              // appbar
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 20, 25, 12),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: const [
-                        Text(
-                          'My',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // appbar
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 20, 25, 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Text(
+                            'My',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        Text(
-                          ' Cards',
-                          style: TextStyle(
-                            fontSize: 30,
+                          Text(
+                            ' Cards',
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
                           ),
+                        ],
+                      ),
+
+                      // plus button
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MyTransitionNavigation()
+                              .createRoute(page: const AddBankAccountPage()));
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFE7EEFC),
+                          ),
+                          child: const Icon(Icons.add),
                         ),
-                      ],
-                    ),
-
-                    // plus button
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MyTransitionNavigation()
-                            .createRoute(page: const AddBankAccountPage()));
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFFE7EEFC),
-                        ),
-                        child: const Icon(Icons.add),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: heigthSize),
-
-              // cards
-              SizedBox(
-                height: 200,
-                child: ListView(
-                  controller: _controller,
-                  scrollDirection: Axis.horizontal,
-                  physics: const BouncingScrollPhysics(),
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  children: [
-                    const MyCard(
-                      balance: 5250.20,
-                      cardNumber: 12345678,
-                      expiryMonth: 10,
-                      expiryYear: 24,
-                      color: Color(0xFF5f54f2),
-                    ),
-                    const MyCard(
-                      balance: 342.23,
-                      cardNumber: 91112131,
-                      expiryMonth: 11,
-                      expiryYear: 23,
-                      color: Color(0xFF8dbfd3),
-                    ),
-                    MyCard(
-                      balance: 420.42,
-                      cardNumber: 14151617,
-                      expiryMonth: 8,
-                      expiryYear: 22,
-                      color: Colors.green[300],
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: heigthSize * .75),
-
-              Center(
-                child: SmoothPageIndicator(
-                  controller: _controller,
-                  count: 3,
-                  effect: ExpandingDotsEffect(
-                      activeDotColor: Colors.grey.shade700,
-                      dotHeight: 6,
-                      expansionFactor: 1.8),
-                ),
-              ),
-
-              const SizedBox(height: heigthSize),
-
-              // 3 buttons -> send + pay + bills
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // send button
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MyTransitionNavigation()
-                            .createRoute(page: const SendPage()));
-                      },
-                      child: const MyButton(
-                        buttonText: 'Send',
-                        iconImagePath: 'lib/icons/send-money.png',
-                      ),
-                    ),
-
-                    // pay button
-                    const MyButton(
-                      buttonText: 'Pay',
-                      iconImagePath: 'lib/icons/credit-card.png',
-                    ),
-
-                    // bills button
-                    const MyButton(
-                      buttonText: 'Bills',
-                      iconImagePath: 'lib/icons/bill.png',
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 8),
-
-              // column -> stats + transactions
-              Container(
-                height: size.height * .4,
-                padding: const EdgeInsets.all(25),
-                child: SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: Column(
-                    children: const [
-                      MyListTile(
-                        iconImagePath: 'lib/icons/estatisticas.png',
-                        tileTitle: 'Statistics',
-                        tileSubTitle: 'Payments and Icome',
-                      ),
-                      MyListTile(
-                        iconImagePath: 'lib/icons/transaction.png',
-                        tileTitle: 'Transactions',
-                        tileSubTitle: 'Transaction History',
-                      )
                     ],
                   ),
                 ),
-              ),
-            ],
+
+                const SizedBox(height: heigthSize),
+
+                // cards
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    controller: _controller,
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    children: [
+                      const MyCard(
+                        balance: 5250.20,
+                        cardNumber: 12345678,
+                        expiryMonth: 10,
+                        expiryYear: 24,
+                        color: Color(0xFF5f54f2),
+                      ),
+                      const MyCard(
+                        balance: 342.23,
+                        cardNumber: 91112131,
+                        expiryMonth: 11,
+                        expiryYear: 23,
+                        color: Color(0xFF8dbfd3),
+                      ),
+                      MyCard(
+                        balance: 420.42,
+                        cardNumber: 14151617,
+                        expiryMonth: 8,
+                        expiryYear: 22,
+                        color: Colors.green[300],
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: heigthSize * .75),
+
+                Center(
+                  child: SmoothPageIndicator(
+                    controller: _controller,
+                    count: 3,
+                    effect: ExpandingDotsEffect(
+                        activeDotColor: Colors.grey.shade700,
+                        dotHeight: 6,
+                        expansionFactor: 1.8),
+                  ),
+                ),
+
+                const SizedBox(height: heigthSize),
+
+                // 3 buttons -> send + pay + bills
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // send button
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MyTransitionNavigation()
+                              .createRoute(page: const SendPage()));
+                        },
+                        child: const MyButton(
+                          buttonText: 'Send',
+                          iconImagePath: 'lib/icons/send-money.png',
+                        ),
+                      ),
+
+                      // pay button
+                      const MyButton(
+                        buttonText: 'Pay',
+                        iconImagePath: 'lib/icons/credit-card.png',
+                      ),
+
+                      // bills button
+                      const MyButton(
+                        buttonText: 'Bills',
+                        iconImagePath: 'lib/icons/bill.png',
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 8),
+
+                // column -> stats + transactions
+                Container(
+                  height: size.height * .4,
+                  padding: const EdgeInsets.all(25),
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      children: const [
+                        MyListTile(
+                          iconImagePath: 'lib/icons/estatisticas.png',
+                          tileTitle: 'Statistics',
+                          tileSubTitle: 'Payments and Icome',
+                        ),
+                        MyListTile(
+                          iconImagePath: 'lib/icons/transaction.png',
+                          tileTitle: 'Transactions',
+                          tileSubTitle: 'Transaction History',
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
